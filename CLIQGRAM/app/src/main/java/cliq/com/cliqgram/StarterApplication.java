d35a45771ae9c6ef7fb95aa3b7218e30d651ef02
Litao(Terry) SHEN <litaoshen0315@gmail.com>
2015-08-23 00:33:07 +1000

    initial push for project skeleton

---
+++ CLIQGRAM/app/src/main/java/cliq/com/cliqgram/StarterApplication.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2015-present, Parse, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+package cliq.com.cliqgram;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+
+public class StarterApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+
+        // Add your initialization code here
+        Parse.initialize(this, "chzaksNDCMU3xDsuty6ml11hlDkEyBqOTaSlWG0I", "9ZQ2NlS94zSHhmTYT5noOCUhXko3bFFD8h4P8bN4");
+
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+
+        ParseUser.enableAutomaticUser();
+        ParseACL defaultACL = new ParseACL();
+        // Optionally enable public read access.
+        // defaultACL.setPublicReadAccess(true);
+        ParseACL.setDefaultACL(defaultACL, true);
+    }
+}
