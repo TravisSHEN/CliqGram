1f0dafea1a2f66e205b4d1b15660276da8551a23
Litao(Terry) SHEN <litaoshen0315@gmail.com>
2015-09-30 16:35:02 +1000

    add viewholder to following activity and youactivity

--- CLIQGRAM/app/src/main/java/cliq/com/cliqgram/viewHolders/FollowingActivityViewHolder.java
+++ CLIQGRAM/app/src/main/java/cliq/com/cliqgram/viewHolders/FollowingActivityViewHolder.java
@@ -3,6 +3,12 @@ package cliq.com.cliqgram.viewHolders;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import cliq.com.cliqgram.R;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by litaoshen on 29/09/2015.
@@ -10,6 +16,15 @@ import android.view.View;
 public class FollowingActivityViewHolder extends RecyclerView.ViewHolder {
 
     private Context context;
+
+    @Bind(R.id.followingactivity_user_avatar)
+    CircleImageView user_avatar;
+    @Bind(R.id.followingactivity_user_name)
+    TextView user_name;
+    @Bind(R.id.followingactivity_text)
+    TextView text;
+    @Bind(R.id.followingactivity_iamge)
+    ImageView image;
 
 
     public FollowingActivityViewHolder(Context context, View itemView) {
