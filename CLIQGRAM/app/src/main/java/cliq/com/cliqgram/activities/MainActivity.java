22a47c4be5be8a9290a96f4ebae72761a394d61c
Quinn Blay <quinn.blay@gmail.com>
2015-10-05 08:42:22 +1100

    Improved formatting.

--- CLIQGRAM/app/src/main/java/cliq/com/cliqgram/activities/MainActivity.java
+++ CLIQGRAM/app/src/main/java/cliq/com/cliqgram/activities/MainActivity.java
@@ -47,24 +47,19 @@ import java.util.Date;
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private static final String NAVIGATION_ITEM_ID = "navigationItemID";
-
+    private static       int    PICKED_IMG         = 1;
+    private static       int    RESULT_LOAD_IMG    = 1;
     @Bind(R.id.mDrawer)
-    DrawerLayout mDrawerLayout;
-
+    DrawerLayout         mDrawerLayout;
     @Bind(R.id.navigation_view)
-    NavigationView navigationView;
-
+    NavigationView       navigationView;
     @Bind(R.id.toolbar)
-    Toolbar toolbar;
-
+    Toolbar              toolbar;
     @Bind(R.id.btn_float_action)
     FloatingActionButton float_button;
-
-    private ActionBarDrawerToggle mDrawerToggle;
-    private int mNavSelectedItemID;
     Button post; //TODO: Specify as either public or private
-    private static int PICKED_IMG = 1;
-    private static int RESULT_LOAD_IMG = 1;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private int                   mNavSelectedItemID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -84,9 +79,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         this.showInitialSelectedFragment(savedInstanceState);
 
         // set up the hamburger icon to open and close the drawer
-        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R
-                .string.drawer_open,
-                R.string.drawer_close);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar,
+                R.string.drawer_open, R.string.drawer_close);
         mDrawerLayout.setDrawerListener(mDrawerToggle);
         mDrawerToggle.syncState();
 
@@ -101,8 +95,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 Intent intent = new Intent();
                 intent.setType("image/*");
                 intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(intent, "Select Picture"),
-                        PICKED_IMG);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICKED_IMG);
             }
         });
     }
@@ -135,8 +128,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             PostService.post(post);
         }
     }
-    private byte[] convertImageToByte(Uri uri){
+
+    private byte[] convertImageToByte(Uri uri) {
         byte[] data = null;
+
         try {
             ContentResolver cr = getBaseContext().getContentResolver();
             InputStream inputStream = cr.openInputStream(uri);
@@ -147,15 +142,17 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+
         return data;
     }
+
     public byte[] getBytes(Uri uri) throws IOException {
-        InputStream inputStream = getContentResolver().openInputStream(uri);
-        ByteArrayOutputStream byteBuffer = new ByteArrayOutputStream();
+        InputStream           inputStream = getContentResolver().openInputStream(uri);
+        ByteArrayOutputStream byteBuffer  = new ByteArrayOutputStream();
 
         // this is storage overwritten on each iteration with bytes
-        int bufferSize = 1024;
-        byte[] buffer = new byte[bufferSize];
+        int    bufferSize = 1024;
+        byte[] buffer     = new byte[bufferSize];
 
         // we need to know how may bytes were read to write them to the byteBuffer
         int len = 0;
@@ -166,6 +163,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         // and then we can return your byte array.
         return byteBuffer.toByteArray();
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -214,70 +212,56 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public boolean onNavigationItemSelected(MenuItem menuItem) {
-
         this.showFragment(menuItem);
-
         return true;
     }
 
 
     //    @OnItemSelected( R.id.navigation_view )
     void showFragment(MenuItem menuItem) {
-
         // close drawer when select one item
         mDrawerLayout.closeDrawers();
 
         menuItem.setChecked(true);
+
         // set current selected navigation item id
         mNavSelectedItemID = menuItem.getItemId();
 
         // open corresponding fragment
         Fragment fragment = null;
-        String title = "";
+        String   title;
 
         switch (menuItem.getItemId()) {
             case R.id.navigation_item_home:
-
                 fragment = FeedFragment.newInstance();
                 title = getString(R.string.navigation_item_home);
-
-                Snackbar.make(toolbar, "Home selected", Snackbar
-                        .LENGTH_SHORT)
-                        .show();
+                Snackbar.make(toolbar, "Home selected", Snackbar.LENGTH_SHORT).show();
                 break;
-            case R.id.navigation_item_profile:
 
+            case R.id.navigation_item_profile:
                 fragment = new ProfileFragment();
                 title = getString(R.string.navigation_item_profile);
-
-                Snackbar.make(toolbar, "Profile selected", Snackbar
-                        .LENGTH_SHORT)
-                        .show();
+                Snackbar.make(toolbar, "Profile selected", Snackbar.LENGTH_SHORT).show();
                 break;
-            case R.id.navigation_item_activity:
 
+            case R.id.navigation_item_activity:
                 fragment = new ActivityFragment();
                 title = getString(R.string.navigation_item_activity);
-
-                Snackbar.make(toolbar, "Activity selected", Snackbar
-                        .LENGTH_SHORT)
-                        .show();
+                Snackbar.make(toolbar, "Activity selected", Snackbar.LENGTH_SHORT).show();
                 break;
-            case R.id.navigation_item_setting:
 
+            case R.id.navigation_item_setting:
                 fragment = new SettingFragment();
                 title = getString(R.string.navigation_item_setting);
-
-                Snackbar.make(toolbar, "Setting selected", Snackbar.LENGTH_SHORT)
-                        .show();
+                Snackbar.make(toolbar, "Setting selected", Snackbar.LENGTH_SHORT).show();
                 break;
 
             default:
+                title = "";
                 break;
         }
 
         if (fragment != null) {
-
             getSupportFragmentManager()
                     .beginTransaction()
                     .replace(R.id.container_body, fragment)
@@ -289,28 +273,25 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
     private void showInitialSelectedFragment(Bundle savedInstanceState) {
-
-        // if there is no pre-selected item,
-        // show home fragment
+        // if there is no pre-selected item, then show home fragment
         if (savedInstanceState == null) {
             mNavSelectedItemID = R.id.navigation_item_home;
         } else {
             mNavSelectedItemID = savedInstanceState.getInt(NAVIGATION_ITEM_ID);
         }
+
         MenuItem menuItem = navigationView.getMenu().findItem(mNavSelectedItemID);
         this.showFragment(menuItem);
     }
 
     @OnLongClick(R.id.btn_float_action)
     boolean onLongClick() {
-        Toast.makeText(this, "Long click", Toast.LENGTH_SHORT)
-                .show();
+        Toast.makeText(this, "Long click", Toast.LENGTH_SHORT).show();
         return true;
     }
 
     @OnClick(R.id.btn_float_action)
     void onClick() {
-        Toast.makeText(this, "Short click", Toast.LENGTH_SHORT)
-                .show();
+        Toast.makeText(this, "Short click", Toast.LENGTH_SHORT).show();
     }
 }
